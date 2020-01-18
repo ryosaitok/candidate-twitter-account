83b4e5f1723bf7eb2fda1d3b4d27ad395286c5b3
Ryo Saito <ryosaito@RyonoMacBook-Pro.local>
2020-01-18 14:34:31 +0900

    change batch hour

--- batch.py
+++ batch.py
@@ -1,15 +1,22 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+import os
 from template.batch import collect_new_graduate_engineers, collect_new_graduate_designers
+
+MODE = os.environ['MODE']
 
 
 def execute():
-    now = datetime.now()
-    print('now.hour: {}'.format(now.hour))
-    # TODO: 実装要修正。HerokuにデプロイするとUS時間になるので日本時間+9時間の時間指定している。
-    if now.hour == 15:
+    jp_time = datetime.now()
+    # 本番環境はHerokuのUSタイムゾーンに置く想定なので、9時間足して日本時間判定できるようにする。
+    if MODE == 'PRODUCTION':
+        jp_time += timedelta(hours=9)
+    # Spread Sheetを手動で触っていない時間に動かしたいので、深夜に起動するようにしている。
+    if jp_time.hour == 0:
         collect_new_graduate_engineers.execute()
-    elif now.hour == 16:
+    elif jp_time.hour == 3:
         collect_new_graduate_designers.execute()
+    else:
+        print(f'バッチ起動時間ではなかったので処理終了。現在時間(日本): {jp_time}')
 
 
 if __name__ == "__main__":
